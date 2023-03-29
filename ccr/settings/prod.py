88377422a31f8429a07a46951468e4874ec49c90
corroconruben <info@corroconruben.online>
2023-03-29 16:17:34 +0200

    path dirs for staic test 2

--- ccr/settings/prod.py
+++ ccr/settings/prod.py
@@ -30,5 +30,5 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 # STATICFILES_DIRS = [BASE_DIR / 'static']
