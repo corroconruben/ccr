a8d2d55f9517d8d80f87065d94f6a176718e2c10
corroconruben <info@corroconruben.online>
2023-03-29 16:21:16 +0200

    retest static

--- ccr/settings/prod.py
+++ ccr/settings/prod.py
@@ -29,6 +29,9 @@ DATABASES = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR / 'static')
+# STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static") 
+
 # STATICFILES_DIRS = [BASE_DIR / 'static']
