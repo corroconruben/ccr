2e78349359f3796efbcf5654a2d16a5ea38de6a0
corroconruben <info@corroconruben.online>
2023-03-29 23:16:45 +0200

    web static ?

--- ccr/settings/prod.py
+++ ccr/settings/prod.py
@@ -37,10 +37,10 @@ DATABASES = {
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/static'),
+    os.path.join(BASE_DIR, 'web/static'),
 ]
 STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # STATICFILES_DIRS = [BASE_DIR / 'static']
